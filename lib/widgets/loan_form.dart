730fc5233084b5e3b8589d177ceee97a8df0dfe2
asuurn <117401182+asuurn@users.noreply.github.com>
2024-04-24 21:30:21 +0300

    Fix the inaccurate loan amount

--- lib/widgets/loan_form.dart
+++ lib/widgets/loan_form.dart
@@ -35,16 +35,8 @@ class _LoanFormState extends State<LoanForm> {
       final result = await _apiService.requestLoanDecision(
           _nationalId, _loanAmount, _loanPeriod);
       setState(() {
-        int tempAmount = int.parse(result['loanAmount'].toString());
-        int tempPeriod = int.parse(result['loanPeriod'].toString());
-
-        if (tempAmount <= _loanAmount || tempPeriod > _loanPeriod) {
-          _loanAmountResult = int.parse(result['loanAmount'].toString());
-          _loanPeriodResult = int.parse(result['loanPeriod'].toString());
-        } else {
-          _loanAmountResult = _loanAmount;
-          _loanPeriodResult = _loanPeriod;
-        }
+        _loanAmountResult = int.parse(result['loanAmount'].toString());
+        _loanPeriodResult = int.parse(result['loanPeriod'].toString());
         _errorMessage = result['errorMessage'].toString();
       });
     } else {
